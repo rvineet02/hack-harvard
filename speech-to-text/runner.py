e1c31827c166dc6f11fc0edf47e6bbce510acdac
Vineet Raju <40352345+rvineet02@users.noreply.github.com>
2022-10-16 00:59:44 -0400

    modify non-public method signatures

--- speech-to-text/runner.py
+++ speech-to-text/runner.py
@@ -15,7 +15,7 @@ class AssemblyAI:
 		self.endpoint = "https://api.assemblyai.com/v2"
 
 
-	def read_file(self, file, chunk_size=1024):
+	def __read_file(self, file, chunk_size=1024):
 		with open(file, 'rb') as _file:
 			while True:
 					data = _file.read(chunk_size)
@@ -23,13 +23,13 @@ class AssemblyAI:
 							break
 					yield data
 
-	def get_upload_url(self, data):
+	def __get_upload_url(self, data):
 		response = requests.post(f"{self.endpoint}/upload", data=data, headers=self.header)
 		res = response.json()
 		return res['upload_url']
 
 
-	def send_for_transcription(self, audio_url):
+	def __send_for_transcription(self, audio_url):
 		json = {
 			"audio_url": audio_url
 		}
@@ -39,12 +39,12 @@ class AssemblyAI:
 		# get the id and status of the transcription
 		return res['id'], res['status']
 
-	def check_status(self, id):
+	def __check_status(self, id):
 		response = requests.get(f"{self.endpoint}/transcript/{id}", headers=self.header)
 		res = response.json()
 		return res['status']
 
-	def get_paragraphs(self, id):
+	def __get_paragraphs(self, id):
 		# make sure the status is completed
 		while True:
 			status = self.check_status(id)
@@ -64,11 +64,11 @@ class AssemblyAI:
 
 	def get_transcript(self, file, chunck_size=1024):
 		# get the upload url
-		upload_url = self.get_upload_url(self.read_file(file, chunck_size))
+		upload_url = self.__get_upload_url(self.read_file(file, chunck_size))
 		# send the file for transcription
-		id, status = self.send_for_transcription(upload_url)
+		id, status = self.__send_for_transcription(upload_url)
 		# get the paragraphs
-		paras = self.get_paragraphs(id)
+		paras = self.__get_paragraphs(id)
 		return paras[0]
 
 if __name__ == "__main__":
